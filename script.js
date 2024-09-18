a635482e051343ee3ef5d6cb797bd576b27018f3
Jayeeh <erroljustinecasbadillo23@gmail.com>
2024-09-19 01:34:51 +0800

    Helpful Update for easy to understand code.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// Initialize the vote counter with a starting value of 3
 let counter = 3;
 
+// Function to handle upvoting
 function upvote() {
-  counter++;
+  counter++; // Increment the counter
+  // Update the displayed number of votes
   document.getElementById("votes").innerText = counter + " Votes";
 }
 
+// Function to handle downvoting
 function downvote() {
-  counter--;
+  counter--; // Decrement the counter
+  // Update the displayed number of votes
   document.getElementById("votes").innerText = counter + " Votes";
 }
